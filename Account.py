656410731d40b78a30236946eeb7444d5c1aba6a
João Sá <joao.vsaa90@gmail.com>
2025-02-10 17:34:38 -0300

    add: solved question one

--- Account.py
+++ Account.py
@@ -7,10 +7,13 @@ class Account:
             self.balance = 0.00
 
     def deposit(self, depositAmount):
-        if self.balance > 0.00:
+        if depositAmount > 0.00:
             self.balance += depositAmount
 
     # Implements withdraw
+    def withdraw(self, withdrawAmount):
+        if withdrawAmount <= self.balance:
+            self.balance -= withdrawAmount
 
 account1 = Account(name="Jane Green", balance=64.00)
 account2 = Account(name="John Blue", balance=-6.00)
@@ -19,12 +22,31 @@ print(account1.name + " balance: " + str(account1.balance))
 print(account2.name + " balance: " + str(account2.balance))
 
 depositAmount = float(input("Enter the amount to be deposited in " + account1.name + " account: "))
-account1.deposit(depositAmount)
-print("Adding " + str(depositAmount) + " to account balance.")
+if depositAmount < 0.00:
+    print("Value invalid!")
+else:
+    print("Adding " + str(depositAmount) + " to account balance.")
+    account1.deposit(depositAmount)
 
 print(account1.name + " balance: " + str(account1.balance))
 print(account2.name + " balance: " + str(account2.balance))
 
 depositAmount = float(input("Enter the amount to be deposited in " + account2.name + " account: "))
-account2.deposit(depositAmount)
-print("Adding " + str(depositAmount) + " to account balance.")
+if depositAmount < 0.00:
+    print("Value invalid!")
+else:
+    print("Adding " + str(depositAmount) + " to account balance.")
+    account2.deposit(depositAmount)
+
+print(account1.name + " balance: " + str(account1.balance))
+print(account2.name + " balance: " + str(account2.balance))
+
+withdrawAmount = float(input("Enter the amount to be withdrawn in " + account1.name + " account: "))
+if withdrawAmount > account1.balance:
+    print("Withdraw value exceeded account balance.")
+else:
+    account1.withdraw(withdrawAmount)
+    print("Removing " + str(withdrawAmount) + " from account balance.")
+
+print(account1.name + " balance: " + str(account1.balance))
+print(account2.name + " balance: " + str(account2.balance))
